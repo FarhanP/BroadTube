5bd6717572f6f571f36a2a4dbacdf6c9123bd125
Mohamed Farhan <pmdfarhan@gmail.com>
2025-08-16 01:50:12 +0530

    changed source path

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Welcome to the BroadTube - where you can freely Broadcast you</div>
+    </>
+  );
+}
+
+export default App;
