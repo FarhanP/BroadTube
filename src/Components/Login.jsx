55760b3baf9bd478dbd339080ba44d826af9732c
Mohamed Farhan <pmdfarhan@gmail.com>
2025-10-22 14:34:01 +0530

    Llogin screen UI update

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -92,7 +92,10 @@ const Login = () => {
   return (
     <div className="flex flex-col gap-15">
       <Header />
-      <div className="flex items-center justify-center pt-20">
+      <h1 className="bg-white/15 backdrop-blur-md border border-white/30 rounded-xl p-4 w-fit m-auto  text-xl sm:text-3xl font-bold text-emerald-400 text-center">
+        Search your favourite Movies
+      </h1>
+      <div className="flex items-center justify-center">
         <form className="border border-gray-700 bg-gray-800 sm:px-8 px-4 sm:py-4 py-2 rounded-lg shadow-lg">
           <div className="flex sm:min-w-sm min-w-xs max-w-xs sm:max-w-sm flex-col gap-8 mx-4 my-8">
             <h2 className="text-white text-3xl text-center">
@@ -129,19 +132,23 @@ const Login = () => {
               <p className="text-red-400 text-sm">{errorMessage}</p>
             )}
             <button
-              className="bg-red-400 text-white hover:opacity-90 font-bold p-3 cursor-pointer"
+              className="bg-red-400 rounded-md text-white hover:opacity-90 font-bold p-3 cursor-pointer"
               onClick={handleSign}
               type="button"
             >
               {isSignedIn ? " Sign In" : "Sign Up"}
             </button>
             <p className="text-white cursor-pointer" onClick={toggleSignIn}>
-              {isSignedIn
-                ? "New to BroadTube? Sign Up"
-                : "Already have an account? Sign In"}
-              {/* <span className="underline cursor-pointer" onClick={toggleSignIn}>
-                {isSignedIn ? " Sign Up" : " Sign In"}
-              </span> */}
+              {isSignedIn ? (
+                <>
+                  New to BroadTube? <span className="font-bold">Sign Up</span>
+                </>
+              ) : (
+                <>
+                  Already have an account?{" "}
+                  <span className="font-bold">Sign In</span>
+                </>
+              )}
             </p>
           </div>
         </form>
